608de76782a4dee0af5892eb2d62d442309ed989
bumaociyuan <zx19880528@gmail.com>
2015-03-28 16:00:08 +0800

    finished lecture 14

--- Trax/Trax/ViewController.swift
+++ Trax/Trax/ViewController.swift
@@ -42,6 +42,69 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.showAnnotations(waypoints, animated: true)
     }
     
+    // MARK: - Constants
+    
+    private struct Constants {
+        static let PartialTrackColor = UIColor.greenColor()
+        static let FullTrackColor = UIColor.blueColor().colorWithAlphaComponent(0.5)
+        static let TrackLineWidth: CGFloat = 3.0
+        static let ZoomCoolDown = 1.5
+        static let LeftCalloutFrame = CGRect(x: 0, y: 0, width: 59, height: 59)
+        static let AnnotationViewReuseIdentifier = "waypoint"
+        static let ShowImageSegue = "Show Image"
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var view = mapView.dequeueReusableAnnotationViewWithIdentifier(Constants.AnnotationViewReuseIdentifier)
+        if  view == nil {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: Constants.AnnotationViewReuseIdentifier)
+            view.canShowCallout = true
+        } else {
+            view.annotation = annotation
+        }
+        
+        view.leftCalloutAccessoryView = nil
+        view.rightCalloutAccessoryView = nil
+
+        if let waypoint = annotation as? GPX.Waypoint {
+            if waypoint.thumbnailURL != nil {
+                view.leftCalloutAccessoryView = UIImageView(frame: Constants.LeftCalloutFrame)
+            }
+            
+            if waypoint.imageURL != nil {
+                view.rightCalloutAccessoryView = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as UIButton
+            }
+        }
+        return view
+    }
+    
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+        performSegueWithIdentifier(Constants.ShowImageSegue, sender: view)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == Constants.ShowImageSegue {
+            if let waypoint = (sender as? MKAnnotationView)?.annotation as? GPX.Waypoint {
+                if let ivc = segue.destinationViewController as? ImageViewController {
+                    ivc.imageURL = waypoint.imageURL
+                    ivc.title = waypoint.name
+                }
+            }
+        }
+    }
+    
+    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
+        if let waypoint = view.annotation as? GPX.Waypoint {
+            if let thumbnailImageView = view.leftCalloutAccessoryView as? UIImageView {
+                if let imageData = NSData(contentsOfURL: waypoint.thumbnailURL!) {
+                    if let image = UIImage(data: imageData) {
+                        thumbnailImageView.image = image
+                    }
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let center = NSNotificationCenter.defaultCenter()
@@ -52,7 +115,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
                 self.gpxURL = url
             }
         }
+        
+         gpxURL = NSURL(string: "http://cs193p.stanford.edu/Vacation.gpx") // for demo/debug/testing
     }
-
 }
 
